7bcefc252716cf7a00d14b9a220ae064255b1a59
palbi <pablo.l.balbi@gmail.com>
2018-08-04 19:57:12 -0300

    Finished migrating CopySceneAssetsToGPU

--- Renderer.cpp
+++ Renderer.cpp
@@ -69,3 +69,172 @@ HRESULT Renderer::LoadSceneAssets() {
 
 	return S_OK;
 }
+
+HRESULT Renderer::CopySceneAssetsToGPU(_In_ ID3D11Device* pd3dDevice) {
+	// Return code, necessary for V_RETURN macro
+	HRESULT hr = S_OK;
+
+	// Create buffer to hold vertex shader transorms (view, world and projection matrixes)
+	CD3D11_BUFFER_DESC cbDesc(sizeof(Renderer::ShaderTransforms), D3D11_BIND_CONSTANT_BUFFER, D3D11_USAGE_DEFAULT);
+	V_RETURN(pd3dDevice->CreateBuffer(&cbDesc, nullptr, &this->vsTransformsBuffer));
+
+	// Define the input layout
+	
+	DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#ifdef _DEBUG
+	// Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
+	// Setting this flag improves the shader debugging experience, but still allows 
+	// the shaders to be optimized and to run exactly the way they will run in 
+	// the release configuration of this program.
+	dwShaderFlags |= D3DCOMPILE_DEBUG;
+
+	// Disable optimizations to further improve shader debugging
+	dwShaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+	// Compile the pixel shader
+	ID3DBlob* pPixelShaderBlob = nullptr;
+	V_RETURN(DXUTCompileFromFile(L"BasicShaders.hlsl", nullptr, "PS", "ps_4_0", dwShaderFlags, 0, &pPixelShaderBlob));
+	// Compile the vertex shader
+	ID3DBlob* pVertexShaderBlob = nullptr;
+	V_RETURN(DXUTCompileFromFile(L"BasicShaders.hlsl", nullptr, "VS", "vs_4_0", dwShaderFlags, 0, &pVertexShaderBlob));
+
+	// Create IA Layout object
+	V_RETURN(pd3dDevice->CreateInputLayout(Renderer::MESH_IA_LAYOUT, Renderer::MESH_IA_LAYOUT_SIZE, 
+		pVertexShaderBlob->GetBufferPointer(), pVertexShaderBlob->GetBufferSize(), &this->pInputLayout));
+
+	// Create shader objects
+	V_RETURN(pd3dDevice->CreateVertexShader(pVertexShaderBlob->GetBufferPointer(), pVertexShaderBlob->GetBufferSize(), nullptr, &this->pVertexShader));
+	V_RETURN(pd3dDevice->CreatePixelShader(pPixelShaderBlob->GetBufferPointer(), pPixelShaderBlob->GetBufferSize(), nullptr, &this->pPixelShader));
+
+	// Change this, have one mesh only
+	V_RETURN(this->sampleMesh.Create(pd3dDevice, L"models\\abstract.sdkmesh"));
+
+	return hr;
+}
+
+void CALLBACK Renderer::HandleFrameRender(_In_ ID3D11Device* pd3dDevice, _In_ ID3D11DeviceContext* pd3dImmediateContext, _In_ double fTime, _In_ float fElapsedTime, _In_opt_ void* pUserContext) {
+	HRESULT hr = S_OK;
+
+	RenderData *pRender = &g_RenderData;
+	ID3D11RenderTargetView *rtv = DXUTGetD3D11RenderTargetView();
+
+	pd3dImmediateContext->ClearRenderTargetView(rtv, DirectX::Colors::LightBlue);
+
+	// Clear the depth stencil
+	auto pDSV = DXUTGetD3D11DepthStencilView();
+	pd3dImmediateContext->ClearDepthStencilView(pDSV, D3D11_CLEAR_DEPTH, 1.0, 0);
+
+	RECT r = DXUTGetWindowClientRect();
+	pRender->transforms.World = XMMatrixIdentity();
+	XMVECTOR Eye = XMVectorSet(0.0f, 0.0f, -5.f, 0.0f) + g_RenderData.Eye;
+	XMVECTOR To = g_RenderData.CameraToDirection;
+	XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	pRender->transforms.View = XMMatrixLookToLH(Eye, To, Up);
+	pRender->transforms.Projection = XMMatrixPerspectiveFovLH(XM_PIDIV2, r.right / (FLOAT)r.bottom, 0.01f, 100.0f);
+
+	// By convention, matrixes are stored in a column-major manner
+	pRender->transforms.World = XMMatrixTranspose(pRender->transforms.World);
+	pRender->transforms.View = XMMatrixTranspose(pRender->transforms.View);
+	pRender->transforms.Projection = XMMatrixTranspose(pRender->transforms.Projection);
+	pd3dImmediateContext->UpdateSubresource(pRender->CBuffer, 0, nullptr, &pRender->transforms, 0, 0);
+
+	UINT Strides[1];
+	UINT Offsets[1];
+	ID3D11Buffer* pVB[1];
+	pVB[0] = pRender->sampleMesh.GetVB11(0, 0);
+	Strides[0] = (UINT)pRender->sampleMesh.GetVertexStride(0, 0);
+	Offsets[0] = 0;
+	pd3dImmediateContext->IASetVertexBuffers(0, 1, pVB, Strides, Offsets);
+	pd3dImmediateContext->IASetIndexBuffer(pRender->sampleMesh.GetIB11(0), pRender->sampleMesh.GetIBFormat11(0), 0);
+
+	D3D11_VIEWPORT viewports[1] = { 0, 0, (FLOAT)r.right, (FLOAT)r.bottom, 0.0f, 1.0f };
+	ID3D11RenderTargetView *rtvViews[1] = { rtv };
+
+	pd3dImmediateContext->IASetInputLayout(pRender->InputLayout);
+	pd3dImmediateContext->VSSetShader(pRender->BasicVS, nullptr, 0);
+	pd3dImmediateContext->VSSetConstantBuffers(0, 1, &pRender->CBuffer.p);
+	pd3dImmediateContext->RSSetViewports(1, viewports);
+	pd3dImmediateContext->PSSetShader(pRender->BasicPS, nullptr, 0);
+	pd3dImmediateContext->OMSetRenderTargets(1, rtvViews, pDSV);
+
+	for (UINT subset = 0; subset < pRender->sampleMesh.GetNumSubsets(0); ++subset) {
+		auto pSubset = pRender->sampleMesh.GetSubset(0, subset);
+		auto PrimType = pRender->sampleMesh.GetPrimitiveType11((SDKMESH_PRIMITIVE_TYPE)pSubset->PrimitiveType);
+
+		pd3dImmediateContext->IASetPrimitiveTopology(PrimType);
+
+		// Ignores most of the material information in them mesh to use
+		// only a simple shader
+		auto pDiffuseRV = pRender->sampleMesh.GetMaterial(pSubset->MaterialID)->pDiffuseRV11;
+
+		pd3dImmediateContext->PSSetShaderResources(0, 1, &pDiffuseRV);
+		pd3dImmediateContext->DrawIndexed((UINT)pSubset->IndexCount, 0, (UINT)pSubset->VertexStart);
+	}
+}
+
+//--------------------------------------------------------------------------------------
+// Handle key presses
+//--------------------------------------------------------------------------------------
+void CALLBACK Renderer::OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext) {
+	float angleDisplacement = .02f;
+	if (bKeyDown) {
+		switch (nChar) {
+		case VK_LEFT:
+			// Rotate camera in y-axis clockwise
+			g_RenderData.CameraToDirection = RotateYAndNormalize(g_RenderData.CameraToDirection, -angleDisplacement);
+			g_RenderData.LateralDirection = RotateYAndNormalize(g_RenderData.LateralDirection, -angleDisplacement);
+			break;
+		case VK_RIGHT:
+			// Rotate camera in y-axis counter-clockwise
+			g_RenderData.CameraToDirection = RotateYAndNormalize(g_RenderData.CameraToDirection, angleDisplacement);
+			g_RenderData.LateralDirection = RotateYAndNormalize(g_RenderData.LateralDirection, angleDisplacement);
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+//--------------------------------------------------------------------------------------
+// Handle messages to the application
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK Renderer::MsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
+	bool* pbNoFurtherProcessing, void* pUserContext) {
+	float keyDeltaMove = .1f;
+	if (uMsg == WM_CHAR) {
+		switch (wParam) {
+		case 'w':
+			g_RenderData.Eye += g_RenderData.CameraToDirection * keyDeltaMove;
+			break;
+		case 's':
+			g_RenderData.Eye += g_RenderData.CameraToDirection * -keyDeltaMove;
+			break;
+		case 'a':
+			g_RenderData.Eye += g_RenderData.LateralDirection * -keyDeltaMove;
+			break;
+		case 'd':
+			g_RenderData.Eye += g_RenderData.LateralDirection * keyDeltaMove;
+			break;
+		default:
+			break;
+		}
+	}
+
+	return 0;
+}
+
+// Handler called on d3d device destruction. Should release resources
+
+void CALLBACK Renderer::OnD3D11DestroyDevice(void* pUserContext) {
+	DXUTGetGlobalResourceCache().OnDestroyDevice();
+
+	g_RenderData.sampleMesh.Destroy();
+}
+
+
+// Math helper function
+
+XMVECTOR RotateYAndNormalize(XMVECTOR aVector, float anAngle) {
+	return XMVector3Normalize(XMVector3Transform(aVector, XMMatrixRotationY(anAngle)));
+}
--- Renderer.h
+++ Renderer.h
@@ -18,17 +18,46 @@ public:
 private:
 	HRESULT AtlCheck(HRESULT hr);
 	DXUTDeviceSettings deviceSettings;
+
+	// DXUT Callbacks definition
 	HRESULT HandleDeviceCreated(ID3D11Device * pd3dDevice, const DXGI_SURFACE_DESC * pBackBufferSurfaceDesc, 
 		void * pUserContext);
+	void HandleFrameRender(ID3D11Device * pd3dDevice, ID3D11DeviceContext * pd3dImmediateContext,
+		double fTime, float fElapsedTime, void * pUserContext);
+	void OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void * pUserContext);
+	LRESULT MsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, 
+		bool * pbNoFurtherProcessing, void * pUserContext);
+	void OnD3D11DestroyDevice(void * pUserContext);
+
+	// D3D needed collaborators
+	CComPtr<ID3D11InputLayout> pInputLayout;
+	CComPtr<ID3D11VertexShader> pVertexShader;
+	CComPtr<ID3D11PixelShader> pPixelShader;
+
+	// TODO: Change this, temporalily only adding one mesh
+	CDXUTSDKMesh sampleMesh;
+
+	// Create and load assets helper
 	HRESULT LoadSceneAssets();
+	HRESULT CopySceneAssetsToGPU(ID3D11Device * pd3dDevice);
 
+	// Scence collaborators
 	Camera camera;
+	CComPtr<ID3D11Buffer> vsTransformsBuffer;
 
-
+	// Buffer objects containers
 	struct ShaderTransforms {
 		XMMATRIX World;
 		XMMATRIX View;
 		XMMATRIX Projection;
 	};
+
+	// IA layout descriptors
+	static const D3D11_INPUT_ELEMENT_DESC MESH_IA_LAYOUT[] = {
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+	UINT MESH_IA_LAYOUT_SIZE= ARRAYSIZE(Renderer::MESH_IA_LAYOUT);
 
 };
